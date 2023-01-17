9f5e09cde85bc6a0292a1ad0433b074027cab87b
minamikik <mia@sync.dev>
2023-01-18 01:35:04 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aesthetic_predictor',
+    version='1.0',
+    description='',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'numpy'
+    ],
+)
