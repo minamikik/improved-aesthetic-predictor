6ff02a82fa3811c63c7a9d803c36d16d7a99912d
minamikik <mia@sync.dev>
2023-01-19 14:19:50 +0900

    Change to src layout

--- src/aesthetic_predictor/aesthetic_predictor.py
+++ src/aesthetic_predictor/aesthetic_predictor.py
@@ -1,3 +1,5 @@
+import os
+import requests
 import clip
 import torch
 import pytorch_lightning as pl
@@ -5,15 +7,34 @@ import torch.nn as nn
 from PIL import Image
 
 class AestheticPredictor():
-    def __init__(self, model_path = "models/sac+logos+ava1-l14-linearMSE.pth"):
+    def __init__(self, model_name = "sac+logos+ava1-l14-linearMSE.pth"):
+
+        self.cache_dir = "cache"
+        os.makedirs(self.cache_dir, exist_ok=True)
+        self.model_name = model_name
+        self.model_path = os.path.abspath(os.path.join(self.cache_dir, self.model_name))
+        if not os.path.exists(self.model_path):
+            self.download_model()
+
         self.model = MLP(768)  # CLIP embedding dim is 768 for CLIP ViT L 14
-        s = torch.load(model_path)   # load the model you trained previously or the model available in this repo
+        s = torch.load(self.model_path)   # load the model you trained previously or the model available in this repo
         self.model.load_state_dict(s)
         self.model.to("cuda")
         self.model.eval()
 
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.model2, self.preprocess = clip.load("ViT-L/14", device=self.device)  #RN50x64   
+        self.model2, self.preprocess = clip.load("ViT-L/14", device=self.device)  #RN50x64
+
+    def download_model(self):
+        remote = "https://github.com/minamikik/improved-aesthetic-predictor/raw/main/models/"
+        url = remote + self.model_name
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(self.model_path, "wb") as f:
+                f.write(response.content)
+        else:
+            raise Exception("Error downloading the model")
+        return
 
     def predict(self, pil_image: Image):
         image = self.preprocess(pil_image).unsqueeze(0).to(self.device)
