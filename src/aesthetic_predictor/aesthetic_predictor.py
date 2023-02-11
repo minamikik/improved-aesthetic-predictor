2e8f6a0ee76d5d9720c0154a975900aef00484ac
minamikik <mia@sync.dev>
2023-02-12 05:37:20 +0900

    Update aesthetic_predictor.py

--- src/aesthetic_predictor/aesthetic_predictor.py
+++ src/aesthetic_predictor/aesthetic_predictor.py
@@ -7,10 +7,10 @@ import torch.nn as nn
 from PIL import Image
 
 class AestheticPredictor():
-    def __init__(self, model_name = "sac+logos+ava1-l14-linearMSE.pth"):
+    def __init__(self, model_path = "sac+logos+ava1-l14-linearMSE.pth"):
 
-        self.model_name = model_name
-        self.model_path = os.path.abspath(os.path.join(self.model_name))
+        self.model_path = model_path
+        self.model_path = os.path.abspath(os.path.join(self.model_path))
         if not os.path.exists(self.model_path):
             self.download_model()
 
